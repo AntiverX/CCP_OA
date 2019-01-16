2b4873779aae64e6930fe3300fb30f6132942c25
antiver <antiver@foxmail.com>
2019-01-16 10:06:23 +0800

    修改了显示页面显示活动的样式

--- activity/views.py
+++ activity/views.py
@@ -212,55 +212,13 @@ def audit_record(request):
         context['auditor'] = request.user.real_name
         return render(request, "activity/audit_record.html", context=context)
     else:
-        file = request.FILES.get('file')
-        # 上传活动时长记录
-        if file is not None:
-            with transaction.atomic():
-                if request.POST['is_cover'] == "1":
-                    ActivityRecord.objects.all().delete()
-                file_name = BASE_DIR + "/static/cache/" + "activity_record.xlsx"
-                with open(file_name.encode(), "wb+") as destination:
-                    for chunk in file.chunks():
-                        destination.write(chunk)
-                data = pd.read_excel(file_name)
-                columns_list = data.columns.values.tolist()
-                columns_list.remove("学号")
-                columns_list.remove("姓名")
-                columns_list.remove("总时长")
-                for index, row in data.iterrows():
-                    student_id = row["学号"]
-                    real_name = row["姓名"]
-                    for column in columns_list:
-                        # 如果时长是0，就不再添加了
-                        if row[column] == 0:
-                            continue
-                        try:
-                            existing_activity = Activity.objects.get(activity_name=column)
-                            activity_time = existing_activity.activity_time
-                            time_length = existing_activity.time_length
-                        except ObjectDoesNotExist:
-                            activity_time = "1921-07-23"
-                            time_length = row[column]
-                        new_activity_record = ActivityRecord.objects.create(
-                            student_id=student_id,
-                            real_name=real_name,
-                            activity_name=column,
-                            joinTime=activity_time,
-                            activity_time=activity_time,
-                            time_length=time_length,
-                            is_ok=True,
-                            auditor="",
-
-                        )
-                        new_activity_record.save()
-        else:
-            target_id = request.POST['target_id']
-            auditor = request.POST['auditor']
-            selected_record = ActivityRecord.objects.get(id=target_id)
-            selected_record.auditor = auditor
-            selected_record.is_ok = "是"
-            selected_record.save()
-        return HttpResponseRedirect("/activity/manage/audit_record")
+        target_id = request.POST['target_id']
+        auditor = request.POST['auditor']
+        selected_record = ActivityRecord.objects.get(id=target_id)
+        selected_record.auditor = auditor
+        selected_record.is_ok = "是"
+        selected_record.save()
+        return HttpResponseRedirect("/activity/activity_record_manage")
 
 # 记录管理
 def activity_record_manage(request):
@@ -288,7 +246,10 @@ def activity_record_manage(request):
                 columns_list = data.columns.values.tolist()
                 columns_list.remove("学号")
                 columns_list.remove("姓名")
-                columns_list.remove("总时长")
+                try:
+                    columns_list.remove("总时长")
+                except:
+                    pass
                 for index, row in data.iterrows():
                     student_id = row["学号"]
                     real_name = row["姓名"]
@@ -300,9 +261,11 @@ def activity_record_manage(request):
                             existing_activity = Activity.objects.get(activity_name=column)
                             activity_time = existing_activity.activity_time
                             time_length = existing_activity.time_length
+                            auditor = existing_activity.person_in_charge
                         except ObjectDoesNotExist:
                             activity_time = "1921-07-23"
                             time_length = row[column]
+                            auditor = ""
                         new_activity_record = ActivityRecord.objects.create(
                             student_id=student_id,
                             real_name=real_name,
@@ -310,12 +273,12 @@ def activity_record_manage(request):
                             joinTime=activity_time,
                             activity_time=activity_time,
                             time_length=time_length,
-                            is_ok=True,
-                            auditor="",
+                            is_ok=1,
+                            auditor=auditor,
 
                         )
                         new_activity_record.save()
-        return HttpResponseRedirect("/activity/manage/audit_record")
+        return HttpResponseRedirect("/activity/activity_record_manage")
 
 
 # 获取单次活动信息
